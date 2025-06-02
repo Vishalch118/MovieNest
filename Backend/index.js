1bca6aed4985d4f81e7117a4aae87125669a0943
Charugundla Vishal <136834267+Vishalch118@users.noreply.github.com>
2025-06-02 12:02:23 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,10 @@ import authRoutes from './routes/auth.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://movienest-frontend.onrender.com', 
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
